dc27ec2b6505a59e16e8ab7e218caa14f9e06ce4
srrogerio <srrogerio@gmail.com>
2022-10-18 15:30:57 -0300

    Primeira aula

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = 'Rogério Candido'
+var idade = 52
+var idade2 = 10
+var frase = 'Japão é o melhor time do mundo'
+//alert(nome + ' tem ' + idade + ' anos')
+//alert(idade + idade2)
+console.log(nome)
+console.log(idade + idade2)
+console.log(frase.replace('Japão', 'Brasil'))
+//alert(frase.replace('Japão', 'Brasil'))
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase().replace('JAPÃO', 'BRASIL'))
