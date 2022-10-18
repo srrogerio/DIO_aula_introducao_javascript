b1f98eca21173de97851ee4f5c229d6ef0033752
srrogerio <srrogerio@gmail.com>
2022-10-18 16:19:33 -0300

    Aula 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 var nome = 'Rogério Candido'
 var idade = 52
 var idade2 = 10
@@ -10,3 +11,39 @@ console.log(frase.replace('Japão', 'Brasil'))
 //alert(frase.replace('Japão', 'Brasil'))
 console.log(frase.toLowerCase())
 console.log(frase.toUpperCase().replace('JAPÃO', 'BRASIL'))
+*/
+
+//var lista = ['maçã', 'pêra', 'laranja']
+//console.log(lista)
+//console.log(lista[1])
+//alert(lista[1])
+
+//lista.push('uva') // Acrescenta um item ao final da lista
+//console.log(lista)
+
+//lista.pop() // Remove um item do final da lista
+//console.log(lista)
+
+//console.log(lista.length) // Quantidade de elementos na lista
+
+//lista.push('uva')
+//console.log(lista.reverse()) // Inverte a lista
+
+//console.log(lista)
+//console.log(lista.toString()) // Converte array em string
+//console.log(lista.join(' - ')) // Converte array em string e você decide o elemento separador
+
+// Dicionário
+/*
+var fruta = { nome: 'maçã', cor: 'vermelha' }
+console.log(fruta)
+console.log(fruta.nome)
+*/
+
+// Lista de dicionário
+var frutas = [
+  { nome: 'maçã', cor: 'vermelha' },
+  { nome: 'uva', cor: 'roxa' }
+]
+console.log(frutas)
+console.log(frutas[1].nome)
